c68d0d4ed7c38e74a2b5d8aab3cdb0ab53302c01
skflowne <hug.geoffrey@gmail.com>
2020-06-30 18:53:38 +0000

    Working cli for creating PDFs from static HTML files

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,7 +1,5 @@
 FROM gitpod/workspace-full
 
-RUN cd /workspace/covid-edu-challenge && yarn
-
 # Install custom tools, runtimes, etc.
 # For example "bastet", a command-line tetris clone:
 # RUN brew install bastet
--- src/cli/index.js
+++ src/cli/index.js
@@ -1,5 +1,75 @@
-import arg from "arg"
+import arg from "arg";
+import inquirer from "inquirer";
+import chalk from "chalk";
+import path from "path";
+import { createPDFFromStaticHTML } from "../pdf/from-static-html";
 
-export function cli(args){
-    console.log(args)
+function parseArguments(rawArgs) {
+  const args = arg({
+    "--html": String,
+    "--out": String,
+  });
+
+  return {
+    htmlZipPath: args["--html"],
+    outputPath: args["--out"],
+  };
+}
+
+async function promptForMissingOptions(options) {
+  const questions = [];
+  if (!options.htmlZipPath) {
+    questions.push({
+      type: "input",
+      name: "htmlZipPath",
+      message: "Enter path of the static HTML zip file",
+      validate: (input) => {
+        return path.extname(input) === ".zip"
+          ? true
+          : "Must point to a valid zip file containing the HTML file";
+      },
+    });
+  }
+
+  const defaultOutputPath = path.join("output", `PDF-HTML-${Date.now()}.pdf`);
+  if (!options.outputPath) {
+    questions.push({
+      type: "input",
+      name: "outputPath",
+      message: "Enter path of desired output",
+      default: defaultOutputPath,
+      filter: (input) => {
+        if (path.extname(input) !== ".pdf") {
+          return `${input}.pdf`;
+        } else {
+          return input;
+        }
+      },
+      validate: (input) => {
+        return path.extname(input) === ".pdf";
+      },
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    ...answers,
+  };
+}
+
+export async function cli(args) {
+  let options = parseArguments(args);
+  try {
+    options = await promptForMissingOptions(options);
+    try {
+      console.log(chalk.bgBlue.white.bold("INPUT HTML"), options.htmlZipPath);
+      console.log(chalk.bgMagenta.white.bold("OUTPUT PDF"), options.outputPath);
+      await createPDFFromStaticHTML(options.htmlZipPath, options.outputPath);
+    } catch (e) {
+      console.error("Failed to create PDF", e);
+    }
+  } catch (e) {
+    console.error("Failed to parse arguments", e);
+  }
 }
